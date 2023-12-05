5f8ed8d4d6407f139251ba9bd6cf4a667cb4b477
YoussefOS2510 <youssefosamame@gmail.com>
2023-12-05 02:17:10 +0200

    ..

--- app/src/main/java/com/example/myapplication/SearchFragment.java
+++ app/src/main/java/com/example/myapplication/SearchFragment.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
+import com.example.myapplication.Classes.Dishes;
+import com.example.myapplication.Classes.ResAdapter;
 import com.example.myapplication.Classes.Restaurants;
+import com.example.myapplication.Classes.SearchAdapter;
 import com.example.myapplication.utils.trie;
 
 import android.content.Intent;
@@ -12,91 +15,93 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import com.example.myapplication.Database.database;
 import com.example.myapplication.Classes.Restaurants;
 public class SearchFragment extends Fragment implements RecyclerViewInterface {
+    private RecyclerView recyclerView;
+    private View rootView;
+    private ArrayList<Restaurants> restaurants;
+    private ArrayList<Dishes> dishes;
+    private ArrayList<Object> SearchItems = new ArrayList<>();
+    private ArrayList<Object> itemList = new ArrayList<>(); // Add this line
+
+    private SearchView searchView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        rootView = inflater.inflate(R.layout.fragment_search, container, false);
 
-        EditText editText = view.findViewById(R.id.searchInput);
-        ListView listView = view.findViewById(R.id.list_view); // Assuming you have a ListView in your layout for suggestions
+        restaurants = Restaurants.getRestaurants();
+        dishes = Dishes.getDishes();
 
+        SearchItems.addAll(restaurants);
+        SearchItems.addAll(dishes);
 
+        itemList.addAll(SearchItems); // Initialize itemList with SearchItems
 
-        Restaurants res1 = new Restaurants("one","12",23,R.drawable.arabiata);
-        Restaurants res2 = new Restaurants("oned","12",23,R.drawable.arabiata);
-        Restaurants res3 = new Restaurants("oner","12",23,R.drawable.arabiata);
-        database.restaurants.add(res1);
-        database.restaurants.add(res2);
-        database.restaurants.add(res3);
+        recyclerView = rootView.findViewById(R.id.SearchView);
 
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(new SearchAdapter(getContext(), SearchItems, this));
 
-//        displaySuggestions(database.restaurants, listView);
-        editText.addTextChangedListener(new TextWatcher() {
+        searchView = rootView.findViewById(R.id.SearchBar);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
-
-
+            public boolean onQueryTextSubmit(String query) {
+                return false;
             }
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
-                String enteredText = charSequence.toString();
-                if(enteredText.isEmpty()){
-                    clearSuggestions(listView);
-//                    displaySuggestions(database.restaurants, listView);
-
-                }
-             else if(!enteredText.isEmpty()){
-                  ArrayList<String> suggestions = trie.findWordsWithPrefix(enteredText);
-
-//                  displaySuggestions(suggestions, listView);
-
-              }else{
-
-              }
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
+            public boolean onQueryTextChange(String newText) {
+                filterlist(newText);
+                return true;
             }
         });
 
-        return view;}
+        return rootView;
+    }
 
-
-//    private void displaySuggestions(ArrayList<Restaurants> suggestions, ListView listView) {
-//        if (getContext() != null) {
-//            RestaurantsAdapter adapter = new RestaurantsAdapter(getContext(), suggestions);
-//            listView.setAdapter(adapter);
-//        }
-//    }
-
-    private void clearSuggestions(ListView listView)    {
-        if (getContext() != null) {
-            ArrayAdapter<String> emptyAdapter = new ArrayAdapter<>(
-                    getContext(),
-                    android.R.layout.simple_list_item_1,
-                    new ArrayList<>()
-            );
-            listView.setAdapter(emptyAdapter);
+    private void filterlist(String newText) {
+        itemList.clear();
+        for (Object object : SearchItems) {
+            if (object instanceof Restaurants) {
+                if (((Restaurants) object).getName().toLowerCase().contains(newText.toLowerCase())) {
+                    itemList.add(object);
+                }
+            } else if (object instanceof Dishes) {
+                if (((Dishes) object).getName().toLowerCase().contains(newText.toLowerCase())) {
+                    itemList.add(object);
+                }
+            }
         }
+
+        if (itemList.isEmpty()) {
+            Toast.makeText(getContext(), "No Data Found", Toast.LENGTH_SHORT).show();
+        }
+
+        // Update the adapter with the filtered list
+        ((SearchAdapter) recyclerView.getAdapter()).setFilteredList(itemList);
     }
 
     @Override
     public void onClick(int pos) {
-        Restaurants newObj = database.restaurants.get(pos);
-        Intent intent = new Intent(getActivity(),activity_menu.class);
-        startActivity(intent);
+        // Handle item click if needed
     }
 }
+
+
 
