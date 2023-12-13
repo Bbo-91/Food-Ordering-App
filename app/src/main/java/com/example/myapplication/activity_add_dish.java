185c76bb9c88b12eb693cfe72c30934df03e0079
karensamuel <113302396+karensamuel@users.noreply.github.com>
2023-12-13 17:49:49 +0200

    add dishes (not working)

---
+++ app/src/main/java/com/example/myapplication/activity_add_dish.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import static android.app.PendingIntent.getActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_add_dish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.add_new_dish);
+
+    }
+}
